49b109e23089cb2fcbe7f65ab150dbbaff95d5a2
Majorbangz88 <joelchimaobichukwu@gmail.com>
2024-08-17 01:58:15 +0100

    modified user model

---
+++ models/user.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true, },
+    email: { type: String, required: true, unique: true, },
+    password: { type: String, required: true,},
+    scoreList: [
+        {
+            subject: { type: String },
+            score: Number,
+        }
+    ],
+});
+
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
